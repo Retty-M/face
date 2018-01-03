39bff03b8b4d7d9205eae32698dbd3f7bed5194f
id <qqq3000777@126.com>
2018-01-03 15:05:47 +0800

    Fix Slow Bugs

--- detect_new.py
+++ detect_new.py
@@ -99,8 +99,8 @@ def main(args):
         frame = add_overlays(frame, faces, frame_rate)
 
         frame_count += 1
-        # cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
-        # cv2.setWindowProperty('Video', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty('Video', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
         cv2.imshow('Video', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
